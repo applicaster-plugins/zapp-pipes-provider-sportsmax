f72fe76fe1b1b007b47e1e2282cb6e83b6a6198b
Elad Kariti <e.kariti@applicaster.com>
2019-08-05 20:06:08 +0300

    support url param

--- src/provider/handler/index.js
+++ src/provider/handler/index.js
@@ -2,15 +2,27 @@ import * as commands from './commands';
 
 export const handler = nativeBridge => params => {
   const type = getType(params);  
+  console.log(params);
+  
   if (!type || [ 'generateToken' ].indexOf(type) == -1) {
     return nativeBridge.throwError('unknown request');
   }
-  
+  params = convertParam(params);
   return commands[type](params)
     .then(nativeBridge.sendResponse)
     .catch(nativeBridge.throwError);
 };
 
+function convertParam(params){
+  try{
+    var items = params.url.split("&");
+    params.timestamp = items[0].split("=")[1];
+    params.uuid = items[1].split("=")[1];
+  }catch(e){
+
+  }
+  return params;
+}
 
 
 
